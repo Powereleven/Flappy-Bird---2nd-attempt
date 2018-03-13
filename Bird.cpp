d824219b2eb71f6f3dcf7cac9c4544622049035c
Powereleven <35611875+Powereleven@users.noreply.github.com>
2018-03-13 18:11:57 -0300

    Update Bird.cpp

--- Bird.cpp
+++ Bird.cpp
@@ -1,41 +1,40 @@
 #include "Bird.h"
 
-Bird::Bird()
+Bird::Bird(sf::RenderWindow &a) : app(a)
 {
-t.loadFromFile("images/bird.png");
-buffer.loadFromFile("sounds/flap.wav"); flap.setBuffer(buffer);
-s.setTexture(t);
-s.setOrigin(30,22);
-s.setPosition(x,y);
+	t.loadFromFile("images/bird.png");
+	buffer.loadFromFile("sounds/flap.wav"); flap.setBuffer(buffer);
+	s.setTexture(t);
+	s.setOrigin(30, 22);
+	s.setPosition(x, y);
 }
 
 void Bird::leftClick()
 {
-    if(alive==1)flap.play();
-    dy=-4;
+	if (alive == 1)flap.play();
+	dy = -4;
 }
 
 void Bird::movement()
 {
-        dy+=0.1; if(dy<0) angle = -30; else angle=0; //if bird going up, rotate it -30 degrees
-        y+=dy; //moves down the bird - value got testing
+	dy += 0.1; if (dy<0) angle = -30; else angle = 0; //if bird going up, rotate it -30 degrees
+	y += dy; //moves down the bird - value got testing
 }
 
 void Bird::checkHeight()
 {
-    if(y>700) alive = 0;
+	if (y>700) alive = 0;
 }
 
 void Bird::reset()
 {
-    y=400; dy=0;
-    counter = 0; alive = 1; auxiliar_sound = 1;
+	y = 400; dy = 0;
+	counter = 0; alive = 1; auxiliar_sound = 1;
 }
 
-void Bird::draw(sf::RenderWindow& app)
+void Bird::draw()
 {
-s.setPosition(x,y);
-s.setRotation(angle);
-app.draw(s);
+	s.setPosition(x, y);
+	s.setRotation(angle);
+	app.draw(s);
 }
-
