78a2ccfe22747b550579192ca2c01598e2ce0a50
Powereleven <35611875+Powereleven@users.noreply.github.com>
2018-03-13 18:11:07 -0300

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,22 +5,23 @@
 
 class Bird
 {
-    public:
-        sf::SoundBuffer buffer; sf::Sound flap; //all to do with sound in the same line
-        sf::Texture t; sf::Sprite s; //all to do with image in the same line
-        float dy = 0; // for gravity
-        bool alive = 1;
-        int angle = 0;
-        int counter = 0; // for score
-        float x = 150, y = 400;
-        bool auxiliar_sound = 1;
+public:
+	sf::RenderWindow &app;
+	sf::SoundBuffer buffer; sf::Sound flap; //all to do with sound in the same line
+	sf::Texture t; sf::Sprite s; //all to do with image in the same line
+	float dy = 0; // for gravity
+	bool alive = 1;
+	int angle = 0;
+	int counter = 0; // for score
+	float x = 150, y = 400;
+	bool auxiliar_sound = 1;
 
-        Bird();
-        void checkHeight();
-        void leftClick();
-        void movement();
-        void reset();
-        void draw(sf::RenderWindow &app);
+	Bird(sf::RenderWindow &);
+	void checkHeight();
+	void leftClick();
+	void movement();
+	void reset();
+	void draw();
 };
 
 #endif // BIRD_H
